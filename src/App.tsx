9de20580f070dc39f9731faac8b4657a6bdb3422
nathanflorencio <nflorencio13@gmail.com>
2021-05-20 03:42:27 -0300

    Projeto feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Head, Label, Input, StrongLabel } from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App: React.FC = () => {
+
+const [ valor, setValor ] = useState( 0 );
+const [ porcentagem, setPorcentagem ] = useState ( 10 );
+
+  return(
+    <>
+    <Head>Calculadora de Gorjeta</Head>
+
+    <Label>Quanto deu a conta?</Label>
+    <Input type="number" value={valor} onChange={(e)=>setValor(parseFloat(e.target.value))} />
+
+    <Label>Qual a porcentagem de gorjeta?</Label>
+    <Input type="number" value={porcentagem} onChange={(e)=>setPorcentagem(parseFloat(e.target.value))} />
+
+    <hr/>
+
+    {valor > 0 &&
+      <>
+      <Label>Sub-total: R$ {valor}</Label>
+      <Label>Gorjeta ({porcentagem}%): R$ {(porcentagem/100) * valor}</Label>
+      <StrongLabel>Total: R$ {valor + ((porcentagem/100) * valor)}</StrongLabel>
+      </>
+    }
+    
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/styles.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Head = styled.h1`
+`;
+
+export const Label = styled.p`
+`;
+
+export const Input = styled.input`
+  width: 400px;
+  height: 40px;
+  border: 1px solid #000;
+  padding: 5px 10px;
+  font-size: 16px;
+`;
+
+export const StrongLabel = styled(Label)`
+  font-weight: bold;
+  font-size: 18px;
+`;
